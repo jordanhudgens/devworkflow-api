5822fd2704efd2f5067c1af55ad2d103618e44c4
jordanhudgens <jordan@devcamp.com>
2018-10-10 18:22:03 -0500

    Added a project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_194724) do
+ActiveRecord::Schema.define(version: 2018_10_10_232042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "main_objective"
+    t.bigint "user_id"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,6 +31,8 @@ ActiveRecord::Schema.define(version: 2018_10_10_194724) do
     t.text "auth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
+  add_foreign_key "projects", "users"
 end
