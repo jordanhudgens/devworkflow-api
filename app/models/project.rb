b62d104085468e9b901010b12235f95e614d51f6
jordanhudgens <jordan@devcamp.com>
2018-10-10 18:29:56 -0500

    Updated the models to make the projects work

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :title, :main_objective
+
+  enum status: {
+    active:   0,
+    archived: 1
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_uniqueness_of :email
-
   validates_presence_of :email, :name
+
+  has_many :projects, dependent: :destroy
 end
