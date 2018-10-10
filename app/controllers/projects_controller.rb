f884a7bbcfd43ba57e0d873a2b017c73a610dd90
jordanhudgens <jordan@devcamp.com>
2018-10-10 18:28:27 -0500

    Created the projects controller with the index action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
+  include AuthenticationConcern
+
   def index
+    @projects = @current_user.projects.active
+    render json: @projects, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'projects/index'
+  resources :projects, only: [:index]
   resources :registrations, only: [:create]
   resources :sessions, only: [:create]
   delete :logout, to: 'sessions#logout'
