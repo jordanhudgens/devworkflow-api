d5e08fddd79802bb7c41bd6858d084f6e5bae03c
jordanhudgens <jordan@devcamp.com>
2018-10-15 12:22:39 -0500

    Built callback for building project line items

--- app/models/project.rb
+++ app/models/project.rb
@@ -15,4 +15,27 @@ class Project < ApplicationRecord
   def self.sort_by_last_updated
     order('updated_at DESC')
   end
+
+  after_create :build_project_line_items
+
+  private
+
+    def build_project_line_items
+      line_items = [
+        "Wireframes",
+        "Sitemaps",
+        "User Stories",
+        "UML Diagrams",
+        "Hi-Res Designs",
+        "Logo/Images",
+        "Frontend Implementation",
+        "Backend API",
+        "Automated Tests",
+        "Deployment"
+      ]
+
+      line_items.each do |line_item|
+        self.project_line_items.create!(title: line_item)
+      end
+    end
 end
