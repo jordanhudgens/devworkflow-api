98548b7f794675f8af153e093647b029a7433e4c
jordanhudgens <jordan@devcamp.com>
2018-10-19 22:21:18 -0500

    Updated the project line item serializer

--- app/serializers/project_line_item_serializer.rb
+++ app/serializers/project_line_item_serializer.rb
@@ -1,8 +1,9 @@
 class ProjectLineItemSerializer < ActiveModel::Serializer
   attributes :id,
-            :title,
-            :description,
-            :completed
+             :title,
+             :description,
+             :completed,
+             :project_id
 
   has_one :project
   has_many :check_list_items
