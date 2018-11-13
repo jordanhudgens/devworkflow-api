9a26ab71a9dfd69777829f8f7d75c4ad5a5f58b1
jordanhudgens <jordan@devcamp.com>
2018-11-13 15:48:23 -0700

    Added an endpoint and action for updating check list items

--- app/controllers/check_list_items_controller.rb
+++ app/controllers/check_list_items_controller.rb
@@ -15,6 +15,20 @@ class CheckListItemsController < ApplicationController
     end
   end
 
+  def update
+    @check_list_item = CheckListItem.find(params[:id])
+
+    if @check_list_item.user_id == @current_user.id
+      if @check_list_item.update(check_list_item_params)
+        render json: @check_list_item
+      else
+        render json: @check_list_item.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { status: 401 }
+    end
+  end
+
   private
 
     def check_list_item_params
