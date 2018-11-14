c9afaad79cf30cbec2f77e0bd1a337d69eb14cb3
jordanhudgens <jordan@devcamp.com>
2018-11-13 19:58:12 -0700

    Updated the default scope

--- app/models/check_list_item.rb
+++ app/models/check_list_item.rb
@@ -2,5 +2,5 @@ class CheckListItem < ApplicationRecord
   belongs_to :project_line_item
   belongs_to :user, optional: true
 
-  default_scope order('id ASC')
+  default_scope { order(id: :asc) }
 end
